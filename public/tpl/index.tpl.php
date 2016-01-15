7dff7d4a2df76a386b9b9cd0d83d443116f66ace
Julio Laveriano en SITATEL <julioantonio.lp@gmail.com>
2016-01-15 14:52:14 -0500

    cambio de video en el home

--- public/tpl/index.tpl.php
+++ public/tpl/index.tpl.php
@@ -100,7 +100,7 @@
   </div>
   <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-2">
     <div class="video_institucional">
-      <iframe width="420" height="315" src="https://www.youtube.com/embed/h7YEQcig6m4" frameborder="0" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/H3bcivd9NLQ" frameborder="0" allowfullscreen></iframe>
     </div>
   </div>
 </div>
